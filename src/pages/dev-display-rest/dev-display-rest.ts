8ee37c7c859734166eaade31377d3709f5278397
Hans Hokka <hans.hokka@gmail.com>
2017-12-30 11:10:36 +0200

    Sorting based on tags and distance.

--- src/pages/dev-display-rest/dev-display-rest.ts
+++ src/pages/dev-display-rest/dev-display-rest.ts
@@ -9,6 +9,11 @@ import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/catch';
 import { FireparserProvider } from '../../providers/fireparser/fireparser'
 import * as firebase from 'firebase';
+import { Geolocation, GeolocationOptions, Geoposition, PositionError } from '@ionic-native/geolocation';
+import { GeolocationProvider } from '../../providers/geolocation/geolocation';
+import { reorderArray } from 'ionic-angular/util/util';
+import { AlertController } from 'ionic-angular';
+
 /**
  * Generated class for the DevDisplayRestPage page.
  *
@@ -31,8 +36,28 @@ export class DevDisplayRestPage {
   obj: any;
   txt: string;
   distance: number;
-  constructor(public navCtrl: NavController, public navParams: NavParams, public restProvider: RestProvider, public fireparserProvider: FireparserProvider) {
-    this.getUsers();
+  options: GeolocationOptions;
+  currentPos: Geoposition;
+  latitude: number;
+  longitude: number;
+  timestamp: number;
+  speed: number;
+  result: string;
+  address: string;
+  displayAddress: string;
+  displayAddresses: string;
+  testRadioOpen: boolean;
+  testRadioResult;
+
+  constructor(
+    public navCtrl: NavController,
+    public navParams: NavParams,
+    public restProvider: RestProvider,
+    public fireparserProvider: FireparserProvider,
+    public geolocation: Geolocation,
+    public alertCtrl: AlertController) {
+
+
   }
   toggleGroup(group) {
     if (this.isGroupShown(group)) {
@@ -41,57 +66,150 @@ export class DevDisplayRestPage {
       this.shownGroup = group;
     }
   };
+
   isGroupShown(group) {
     return this.shownGroup === group;
   };
-  /* getUsers() {
+  /* Inputs jobs' coords and calculates distance to current pos. */
+
+  getDistance(x, y) {
+    return this.fireparserProvider.getDistance(x, y);
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad DevDisplayRestPage');
+    this.getUsers();
+
+  }
+
+  sortPrintDataBySelection(printData, selector) {
+    if (selector == 'aidinkieli') {
+      this.printData.sort(function (a, b) {
+        if (a.tag == 1) return -1;
+        if (a.tag > b.tag) return 1;
+        return 0;
+      })
+    }
+    if (selector == 'liikunta') {
+      this.printData.sort(function (a, b) {
+        if (a.tag == 2) return -1;
+        if (a.tag > b.tag) return 1;
+        return 0;
+      })
+    }
+    if (selector == 'musiikki') {
+      this.printData.sort(function (a, b) {
+        if (a.tag == 3) return -1;
+        if (a.tag > b.tag) return 1;
+        return 0;
+      })
+    }
+    if (selector == 'biologia') {
+      this.printData.sort(function (a, b) {
+        if (a.tag == 4) return -1;
+        if (a.tag > b.tag) return 1;
+        return 0;
+      })
+    }
+
+    if (selector == 'distance') {
+      this.printData.sort(function (a, b) {
+        if (a.distance < b.distance) return -1;
+        if (a.distance > b.distance) return 1;
+        return 0;
+      })
+    }
+
+  }
+  getUsers() {
+    this.doRadio();
     this.restProvider.getRemoteData()
       .then(data => {
         this.printData = data;
         this.printData = JSON.parse(this.printData._body);
+        for (let i in this.printData) {
+
+
+
+          this.printData[i].tag = Math.floor(Math.random() * 5);;
+          this.printData[i].distance = this.getDistance(
+
+            this.printData[i].x, this.printData[i].y);
+        }
+        this.sortPrintDataBySelection(this.printData, 'tag');
         console.log('printData: ' + JSON.stringify(this.printData[0]));
-
       });
-  };
- */
-  /* Inputs jobs' coords and calculates distance to current pos. */
-  getDistance(x, y) {
-    return this.fireparserProvider.getDistance(x, y);
-  }
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad DevDisplayRestPage');
-  };
-  getUsers() {
 
-  this.restProvider.getRemoteData()
-    .then(data => {
-      this.printData = data;
-      this.printData = JSON.parse(this.printData._body);
-      this.printData[0].distance = this.getDistance(
-        this.printData[0].x, this.printData[0].y);
-      console.log('printData: ' + JSON.stringify(this.printData[0]));
+
+  };
+
+  getUserPosition() {
+    this.options = {
+      enableHighAccuracy: true
+    };
+    this.geolocation.getCurrentPosition(this.options).then((pos: Geoposition) => {
+
+      this.currentPos = pos;
+      this.latitude = pos.coords.latitude;
+      this.longitude = pos.coords.longitude;
+      this.timestamp = pos.timestamp;
+      this.speed = pos.coords.speed;
+
+      console.log(this.latitude);
+    }, (err: PositionError) => {
+      console.log("error : " + err.message);
+    });
+  }
+  doRadio() {
+    let alert = this.alertCtrl.create();
+    alert.setTitle('Valitse järjestysperuste');
+
+    alert.addInput({
+      type: 'radio',
+      label: 'Etäisyys',
+      value: 'distance',
+      checked: true
+    });
+
+    alert.addInput({
+      type: 'radio',
+      label: 'Äidinkieli',
+      value: 'aidinkieli'
 
     });
 
-    
-};
-getUserPosition() {
-  this.options = {
-    enableHighAccuracy: true
-  };
+    alert.addInput({
+      type: 'radio',
+      label: 'Liikunta',
+      value: 'liikunta'
+    });
 
-  this.geolocation.getCurrentPosition(this.options).then((pos: Geoposition) => {
+    alert.addInput({
+      type: 'radio',
+      label: 'Musiikki',
+      value: 'musiikki'
+    });
 
-    this.currentPos = pos;
-    this.latitude = pos.coords.latitude;
-    this.longitude = pos.coords.longitude;
-    this.timestamp = pos.timestamp;
-    this.speed = pos.coords.speed;
-    //console.log(pos);
+    alert.addInput({
+      type: 'radio',
+      label: 'Biologia',
+      value: 'biologia'
+    });
 
-  }, (err: PositionError) => {
-    console.log("error : " + err.message);
-  });
+    alert.addButton('Peru');
+    alert.addButton({
+      text: 'Ok',
+      handler: data => {
+        console.log('Radio data:', data);
+        this.testRadioOpen = false;
+        this.testRadioResult = data;
+        this.sortPrintDataBySelection(this.printData, data);
+      }
+    });
+
+    alert.present().then(() => {
+      this.testRadioOpen = true;
+    });
+  }
+
 }
-}
-
